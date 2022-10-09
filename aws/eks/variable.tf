bfa852d4a3fab6d84e0d8fea7c65a92cac1fc47b
spike494 <72295925+spike494@users.noreply.github.com>
2022-10-09 21:08:29 +0530

    resolving merge commit

---
+++ aws/eks/variable.tf
@@ -0,0 +1,20 @@
+variable "cluster-service-role" {
+	description = ""
+	default = ""
+}
+variable "node-role" {
+        description = ""
+        default = ""
+}
+variable "aws-region" {
+	description = ""
+	default = ""
+}
+variable "vpc" {
+	description = ""
+	default = ""
+}
+variable "node-subnet" {
+	description = ""
+	default = ""
+}
